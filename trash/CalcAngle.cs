87c54c2eb69f5dbcaa66dde7c88e8c58e5704081
Marcus Weaver <onemangd@gmail.com>
2016-11-11 01:11:53 +0400

    Update CalcAngle.cs

--- trash/CalcAngle.cs
+++ trash/CalcAngle.cs
@@ -5,23 +5,13 @@ public class CalcAngle : MonoBehaviour {
 
 	public static float calcAngleBetween (Vector3 firstOrigin, Vector3 firstEnd, Vector3 secondOrigin, Vector3 secondEnd)
 	{
-		//Косинус угла между векторами равен скалярному произведению векторов, поделенному на произведение модулей векторов.
-		//Сначала находим скалярное произведение
 		Vector3 firstCoordinates = new Vector3 (firstEnd.x - firstOrigin.x, firstEnd.y - firstOrigin.y, firstEnd.z - firstOrigin.z); 
-		//print (firstCoordinates);
 		Vector3 secondCoordinates = new Vector3 (secondEnd.x - secondOrigin.x, secondEnd.y - secondOrigin.y, secondEnd.z - secondOrigin.z); 
-		//print (secondCoordinates);
 		float scalar = (firstCoordinates.x * secondCoordinates.x) + (firstCoordinates.y * secondCoordinates.y) + (firstCoordinates.z * secondCoordinates.z);
-		//print (scalar);
 		float modA = Mathf.Sqrt (Mathf.Pow (firstCoordinates.x, 2) + Mathf.Pow (firstCoordinates.y, 2) + Mathf.Pow (firstCoordinates.z, 2));
-		//print (modA);
 		float modB = Mathf.Sqrt (Mathf.Pow (secondCoordinates.x, 2) + Mathf.Pow (secondCoordinates.y, 2) + Mathf.Pow (secondCoordinates.z, 2));
-		//print (modB);
 		float cosA = scalar/(modA+modB);
-		//print (cosA);
 		float AcosA = Mathf.Acos (cosA);
-		//print (AcosA);
-		//print (AcosA * Mathf.Rad2Deg);
 		return AcosA*Mathf.Rad2Deg;
 	}
 
